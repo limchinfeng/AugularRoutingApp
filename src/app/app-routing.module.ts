9a5dca07c1d53701618c2a3d45697e6433a43c74
supershuaifeng <limchinfeng@gmail.com>
2023-12-30 15:03:35 +0800

    Not Found pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  {path: 'first', component: FirstComponent},
-  {path: 'second', component: SecondComponent}
+  {path: '', component: FirstComponent},
+  {path: 'second', component: SecondComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
